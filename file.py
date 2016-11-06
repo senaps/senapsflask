b6ab69340c981e849078fb1249bb7709f17657ca
Senaps <gerdakan.sa@gmail.com>
2016-11-06 21:43:38 +0330

    added templating functionality!

--- file.py
+++ file.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'hello world!'
+    mytitle = 'this is from code!'
+    return render_template('index.html', title= mytitle)
+
+@app.route('/contact')
+def contact():
+    title = 'contact form!'
+    return render_template('contact.html', title= title)
+
+@app.route('/about')
+def about():
+    title = 'about us!'
+    return render_template('about.html', title= title)
+
 
 if __name__ == '__main__':
     app.run()
