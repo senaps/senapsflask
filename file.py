5bd83f259a81e5554ae18dbe791ab2c6d5a18b9e
Senaps <gerdakan.sa@gmail.com>
2016-11-12 22:55:16 +0330

    created a flash! :)

--- file.py
+++ file.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, session, redirect
+from flask import Flask, render_template, url_for, request, session, redirect, flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY']= 'youd never guessed it! :)'
@@ -35,11 +35,12 @@ def login_success():
     if request.method == 'POST':
         session['name'] = request.form['name']
         session['log_in'] = True
+        flash('your name is set!')
         return redirect(url_for('index'))
 
 @app.route('/logout')
 def log_out():
-    session['name']= None
+    session.pop('name')
     session['log_in']= False
     return redirect(url_for('index'))
 
